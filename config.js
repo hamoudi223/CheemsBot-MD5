54be9ee52ed1eb95b1a82389175a3a600d5941e1
Seif-01 <117954932+Seif-01@users.noreply.github.com>
2022-11-11 09:09:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['212689265072'] //ur owner number
+global.ownername = " 𝕊𝕖𝕚𝕗 𝔹𝕠𝕥 🍜🦊 " //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
+global.owner = ['212689265072'] //ur number
+global.ownernomer = "212689265072" //ur number
+global.ownertag = '212689265072' //ur tag number
+global.premium = ['212689265072'] //ur premium number
 global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.ownername = " 𝕊𝕖𝕚𝕗 𝔹𝕠𝕥 🍜🦊 "
+global.linkz = "www.youtube.com" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
+global.themeemoji = "🍜" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
 global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
